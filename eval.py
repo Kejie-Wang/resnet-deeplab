fae517efb7dddcfd690b744d58afd08bfe1b102e
Kejie-Wang <wang_kejie@foxmail.com>
2018-02-01 20:26:51 +0800

    update eval

--- eval.py
+++ eval.py
@@ -38,45 +38,85 @@ def main():
 
     dataset = reader.Dataset(args.data_dir, subset='validation')
     dataset.make_batch(batch_size=1, epoch_size=1, shuffle=False)
-    image, label = dataset.next_batch()
+    image_batch, labels_batch = dataset.next_batch()
 
-    model = resnet_deeplab.Resnet101Deeplab(image, args.num_classes)
+    model = resnet_deeplab.Resnet101Deeplab(image_batch, args.num_classes)
 
     restore_var = tf.global_variables()
 
-    prediction = model.prediction
-    # upsampling the prediction with the same size with label
-    prediction = tf.image.resize_bilinear(prediction, tf.shape(image)[1:3])
-    prediction = tf.argmax(prediction, axis=3)
-    # flatten the prediction and label.
-    prediction = tf.reshape(prediction, [-1])
-    label = tf.reshape(label, [-1])
+    # upsampling the predictions with the same size with labels
+    predictions = tf.image.resize_bilinear(model.prediction,
+                                           tf.shape(image_batch)[1:3])
+    predictions = tf.argmax(predictions, axis=3)
+    # flatten the predictions and labels.
+    predictions = tf.reshape(predictions, [-1])
+    labels = tf.reshape(labels_batch, [-1])
 
-    boolean_mask = tf.less(label, args.num_classes)
-    prediction = tf.boolean_mask(prediction, boolean_mask)
-    label = tf.boolean_mask(label, boolean_mask)
+    boolean_mask = tf.less(labels, args.num_classes)
+    predictions = tf.boolean_mask(predictions, boolean_mask)
+    labels = tf.boolean_mask(labels, boolean_mask)
 
-    # weights = tf.cast(tf.less(label, args.num_classes), tf.float32)
+    predictions = tf.cast(predictions, tf.int32)
+    labels = tf.cast(labels, tf.int32)
+    corr_pred_pixel = tf.reduce_sum(
+        tf.cast(tf.equal(predictions, labels), tf.int32))
+    wrong_pred_pixel = tf.reduce_sum(
+        tf.cast(tf.not_equal(predictions, labels), tf.int32))
+
+    # weights = tf.cast(tf.less(labels, args.num_classes), tf.float32)
     mean_iou, update_op = tf.metrics.mean_iou(
-        label, prediction, num_classes=args.num_classes)
+        labels, predictions, num_classes=args.num_classes)
+
+    with tf.name_scope('loss'):
+        prediction_size = tf.shape(model.prediction)[1:3]
+        labels = tf.image.resize_nearest_neighbor(labels_batch,
+                                                  prediction_size)
+        labels = tf.squeeze(
+            labels, squeeze_dims=[3])  # squeeze the last (color) channel.
+        labels = tf.reshape(labels, [-1])
+        # filter the indices which labels exceeds than num class.
+        boolean_mask = tf.less(labels, args.num_classes)
+        labels = tf.boolean_mask(labels, boolean_mask)
+        labels = tf.cast(labels, tf.int32)
+        predictions = tf.reshape(model.prediction, [-1, args.num_classes])
+        predictions = tf.boolean_mask(predictions, boolean_mask)
+        entropy_loss = tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(
+                logits=predictions, labels=labels))
+        l2_loss = tf.add_n([
+            0.005 * tf.nn.l2_loss(v) for v in tf.trainable_variables()
+            if 'weights' in v.name or 'biases' in v.name
+        ])
+        loss = entropy_loss + l2_loss
+
+    all_corr_pred = 0
+    all_wrong_pred = 0
 
     loader = tf.train.Saver(var_list=restore_var)
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     with tf.Session(config=config) as sess:
-        sess.run(tf.global_variables_initializer())
+        # sess.run(tf.global_variables_initializer())
         sess.run(tf.local_variables_initializer())
 
-        loader.restore(sess, args.model_path)
+        path = tf.train.latest_checkpoint('saved_model')
+        loader.restore(sess, path)
 
         for i in range(dataset.num_examples):
-            sess.run(update_op)
-            if i % 10 == 0:
+            _, corr, wrong = sess.run([update_op, corr_pred_pixel, wrong_pred_pixel])
+            all_corr_pred += corr
+            all_wrong_pred += wrong
+            # print(sess.run([entropy_loss, l2_loss, loss]))
+            if i % 100 == 0:
                 INFO(i / dataset.num_examples)
                 INFO('Mean IoU: {:.3f}'.format(mean_iou.eval(session=sess)))
+                INFO('correct:', all_corr_pred, 'all:',
+                     all_corr_pred + all_wrong_pred)
 
         INFO('Mean IoU: {:.3f}'.format(mean_iou.eval(session=sess)))
+        INFO('Pixel accuracy: {:.3f}'.format(all_corr_pred /
+                                             (all_corr_pred + all_wrong_pred)))
 
 
 if __name__ == '__main__':
